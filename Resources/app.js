41bc1ad700176bf9065c8cc2bc3368551d2aea65
Sarp Kaya <sarpkaya@live.com.au>
2017-10-12 13:35:48 +1000

    Database code added

--- Resources/app.js
+++ Resources/app.js
@@ -127,6 +127,103 @@ function getRestaurants(lat, lon) {
     client.setRequestHeader('user-key', '0309287a5838e5' + '15f2c0a39b8143f17c');
     client.send();
 }
+
+
+function initDb() {
+
+    Ti.Database.install('/dbname.sqlite', 'randomMunchDb');
+    var db = Ti.Database.open('randomMunchDb');
+    db.execute('CREATE TABLE IF NOT EXISTS LIKED_RESTAURANTS(id INTEGER PRIMARY KEY, name TEXT);');
+    db.execute('CREATE TABLE IF NOT EXISTS DISLIKED_RESTAURANTS(id INTEGER PRIMARY KEY, name TEXT);');
+    db.execute('CREATE TABLE IF NOT EXISTS SETTINGS(id INTEGER PRIMARY KEY, notificationSeconds INTEGER);');
+    db.execute('INSERT INTO SETTINGS (id,notificationSeconds) VALUES (?,?)', 1, 3600);
+
+    db.close();
+
+}
+
+function updateNotificationSecond(seconds) {
+    var db = Ti.Database.open('randomMunchDb');
+    db.execute('UPDATE SETTINGS SET notificationSeconds = ? WHERE id = 1', seconds);
+    db.close();
+}
+
+function getNotificationSecond() {
+    var db = Ti.Database.open('randomMunchDb');
+
+    var sql = "SELECT notificationSeconds FROM SETTINGS WHERE id = 1";
+    var RS = db.execute(sql);
+
+    var retVal = 3600;
+    while (RS.isValidRow()) {
+        retVal = RS.fieldByName('notificationSeconds');
+        RS.next();
+    }
+    RS.close();
+
+    return retVal;
+}
+
+function getLikedRestaurants() {
+
+    var db = Ti.Database.open('randomMunchDb');
+
+    var sql = "SELECT * FROM LIKED_RESTAURANTS";
+    var RS = db.execute(sql);
+
+
+    var retVal = [];
+    while (RS.isValidRow()) {
+        retVal.push({id: RS.fieldByName('id'), name: RS.fieldByName('name')});
+        RS.next();
+    }
+    RS.close();
+
+    return retVal;
+}
+
+function getDislikedRestaurants() {
+
+    var db = Ti.Database.open('randomMunchDb');
+
+    var sql = "SELECT * FROM DISLIKED_RESTAURANTS";
+    var RS = db.execute(sql);
+
+
+    var retVal = [];
+    while (RS.isValidRow()) {
+        retVal.push({id: RS.fieldByName('id'), name: RS.fieldByName('name')});
+        RS.next();
+    }
+    RS.close();
+
+    return retVal;
+}
+
+function deleteDislikedRestaurant(id) {
+    var db = Ti.Database.open('randomMunchDb');
+    db.execute('DELETE FROM DISLIKED_RESTAURANTS WHERE id=?', id);
+    db.close();
+}
+
+function deleteLikedRestaurant(id) {
+    var db = Ti.Database.open('randomMunchDb');
+    db.execute('DELETE FROM LIKED_RESTAURANTS WHERE id=?', id);
+    db.close();
+}
+
+function addDislikedRestaurant(id, name) {
+    var db = Ti.Database.open('randomMunchDb');
+    db.execute('INSERT INTO DISLIKED_RESTAURANTS (id,name) VALUES (?,?)', id, name);
+    db.close();
+}
+
+function addLikedRestaurant(id, name) {
+    var db = Ti.Database.open('randomMunchDb');
+    db.execute('INSERT INTO LIKED_RESTAURANTS (id,name) VALUES (?,?)', id, name);
+    db.close();
+}
+
 //End of Helper Function
 
 
@@ -149,7 +246,9 @@ function constructMainView(_args) {
     var coords = getCurrentCoordinates();
     console.log("Coords latitude: " + coords.latitude + " and longitude: " + coords.longitude);
     getRestaurants(coords.latitude, coords.longitude);
+
     return mainWin;
 };
 
+initDb();
 constructMainView({title: 'Random Munchies'}).open();
